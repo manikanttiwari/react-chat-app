a0154c7bd7d06afc2f0c99d7bfedaa1f12b0cc5b
manikanttiwari <32715824+manikanttiwari@users.noreply.github.com>
2020-11-05 10:45:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://project:manikant@manikant.uc3ja.mongodb.net/chat?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://project:<username>@<password>.uc3ja.mongodb.net/<databasename>?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
